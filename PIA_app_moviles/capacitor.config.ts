77faecc3da26b31bd98eb6b07b18d0f048c5d32f
Diego Moreno <diegomorduar@gmail.com>
2024-05-06 17:09:35 -0600

    Creación del proyecto

---
+++ PIA_app_moviles/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PIA_app_moviles',
+  webDir: 'www'
+};
+
+export default config;
