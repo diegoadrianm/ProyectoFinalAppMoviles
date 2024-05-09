2245959feb8b456e9570680ab4b505237728be23
joseenriquez1404 <manuel.enriquezr@uanl.edu.mx>
2024-05-09 10:57:43 -0600

    Conexion a Firebase

--- PIA_app_moviles/src/app/app.module.ts
+++ PIA_app_moviles/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp({"projectId":"pia-appmoviles-bfb3a","appId":"1:334001649967:web:c16fd0c8ef459530ac89da","storageBucket":"pia-appmoviles-bfb3a.appspot.com","apiKey":"AIzaSyDZLgTbVdKBQD6eHSGQBSBbLulMcb9ltog","authDomain":"pia-appmoviles-bfb3a.firebaseapp.com","messagingSenderId":"334001649967"})), provideFirestore(() => getFirestore())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
