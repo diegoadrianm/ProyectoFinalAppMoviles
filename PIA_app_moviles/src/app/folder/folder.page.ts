3bf7845ef85ee223d3e4690e75f23854bcff95fe
joseenriquez1404 <manuel.enriquezr@uanl.edu.mx>
2024-05-20 10:56:55 -0600

    solucion error

--- PIA_app_moviles/src/app/folder/folder.page.ts
+++ PIA_app_moviles/src/app/folder/folder.page.ts
@@ -4,6 +4,11 @@ import { IonModal, ModalController } from '@ionic/angular';
 import { OverlayEventDetail } from '@ionic/core/components';
 import { Usuario } from '../Interfaces/Usuario';
 import { InicioSesionComponent } from '../inicio-sesion/inicio-sesion.component';
+import { Contacto } from '../Interfaces/Contacto';
+import { AgregarContactoComponent } from '../agregar-contacto/agregar-contacto.component';
+import { ContactosService } from '../services/contactos.service';
+import { OpenModalService } from '../services/open-modal.service';
+
 
 
 @Component({
@@ -14,13 +19,25 @@ import { InicioSesionComponent } from '../inicio-sesion/inicio-sesion.component'
 export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
-  constructor(private modalCtrl: ModalController) {}
+  constructor(private modalCtrl: ModalController,private listaContactos: ContactosService, public isModalOpen: OpenModalService) {}
+
 
  
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
   }
+
+
+  contactos: Contacto[] = this.listaContactos.contactos;
+
+  
+  setOpen(state: boolean) {
+    this.isModalOpen.isOpen = state;  
+  }
+  
+
+
 
   sesionIniciada: boolean = false;
 
